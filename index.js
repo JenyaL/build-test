c03847e7d69a9289a49cb98cc620dbab84120303
Yevhen <38507392+JenyaL@users.noreply.github.com>
2025-09-27 04:26:14 -0600

    init: base Node.js project with config

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+    if (process.env.BUILD_RESULT === "success") {
+        console.log("Build success");
+        process.exit(0);
+    }
+
+throw new Error("Build failed");
